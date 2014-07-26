4123d7694a307b8cccfbb22e868cb507a5bbc25c
Alberto Gonzalez <arg124@prodigy.net.mx>
2014-07-26 09:50:52 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,8 @@
 ## Course project "Getting and claning data"
+# This code runs in Windows 7
+# The route is C:/Users/NameUser/Documents/R/run_analysis.R
 #
-# Download raw data
+# Download the file with raw data
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 destfile <- "getdata-projectfiles-UCI-HAR-Dataset.zip"
